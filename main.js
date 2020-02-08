6f0cf9491526d627dd95ee785f9f3da170362aec
Brandon <bmiller3822@gmail.com>
2020-02-08 13:38:18 -0500

    I actually got a new input field to populate for editing a note.  Progress.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let editTextBox = q("#note-text")
+let editInput = q("edit-text")
+var newInput = document.createElement("input")
+
 function q(selector){
     return document.querySelector(selector)
 }
@@ -18,26 +22,42 @@ function createNotesHTML(notes){
     return notesStr
 }
 
-getAllNotes().then(createNotesHTML)
+function editNoteHTML(note){
+    let notesStr = '<ul id="notes-list">'
+    notesStr+=editNoteHTML(note)
+    notesStr += '</ul>'
+    return notesStr
+}
 
 function createNoteHTML(note){
-    return `<li data-note-id="${note.id}">${note.note} <button class="delete">Delete</button></li>`
+    return `<li class="title" data-node-id="${note.id}">${note.title}</li>   <li data-note-id="${note.id}">${note.note} <button class="edit">Edit</button><button class="delete">Delete</button></li>`
+}
+
+function editNoteHTML(note){
+    return `<li data-note-id="${note.id}">${note.note}</li>`
 }
 
 function renderNotesList(notes) {
     const notesHTML = createNotesHTML(notes)
-    const notesSection = document.querySelector('#notes')
+    const notesSection = q('#notes')
     notesSection.innerHTML = notesHTML
+}
+
+function renderEditNote(note){
+    const noteHTML = editNoteHTML(note)
+    const notesSection = q(event.target.parentElement)
+    notesSection.innerHTML = noteHTML
 }
 
 q ("#new-note-form").addEventListener('submit', event=>{
     event.preventDefault();
+    const titleTextField = q('#note-title').value
     const noteTextField = q('#note-text').value  //This line will eventually become multiple functions.  See right below.
     fetch("http://localhost:3000/notes/", {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
-            note:noteTextField,done:false,create:moment().format()})
+            note:noteTextField, title:titleTextField, done:false, create:moment().format()})
     })
 })
 //This is what I was referring to:
@@ -49,16 +69,14 @@ q ("#new-note-form").addEventListener('submit', event=>{
 //     postNewTodo(todoText).then(renderNewTodo)
 //   })
 
-function postNewNote (noteText){
+function postNewNote (titleText, noteText){
     return fetch('http://localhost:3000/notes/',{
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({note:noteText, done: false, created: moment().format()})
+        body: JSON.stringify({note:noteText, title: titleText, note: noteText, done: false, created: moment().format()})
     })
     .then(response=>response.json())
 }
-
-getAllNotes().then(renderNotesList)
 
 function deleteThisNote (noteId){
     return fetch('http://localhost:3000/notes/' + noteId,{
@@ -68,8 +86,6 @@ function deleteThisNote (noteId){
     })
     .then(response=>response.json())
 }
-
-// console.log(document.querySelector(".delete")) 
 
 function deleteNote(){
     q('#notes').addEventListener('click', event=>{
@@ -82,21 +98,69 @@ function deleteNote(){
     )
 }
 
+function editThisNote (noteId){ //addElement??  <li data-note-id="${note.id}">${note.note}</li>
+    return fetch('http://localhost:3000/notes/' + noteId,{
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({note:editedNote.value, done: false, edited: moment().format()})
+    })
+    .then(response=>response.json())
+}
+
+function editNote(){
+    q('#notes').addEventListener('click', event=>{
+        
+        if (event.target.matches('.edit')){
+            event.preventDefault();
+            event.target.parentElement.classList.add("noteToEdit")
+            event.target.parentElement.value=""
+            let editedNote = event.target.appendChild(newInput)
+            editedNote.classList.add("editClass")
+            q(".editClass").addEventListener('click', event=>{
+                console.log(editedNote.value)
+                return editedNote.value
+            })          
+        }   
+        let noteId = (event.target.parentElement.dataset.noteId)
+        editThisNote(noteId)
+    }
+    )
+}
+
+getAllNotes().then(createNotesHTML)
+getAllNotes().then(renderNotesList)
 deleteNote()
+editNote()
 
 console.log("Nothing broken!")
 
+// noteSubmit.addEventListener("submit", event => {
+//     event.preventDefault();
+//     const titleTextField = document.querySelector("#note-title");
+//     const noteTextField = document.querySelector("#note-text");
+//     const titleText = titleTextField.value;
+//     const noteText = noteTextField.value;
+//     titleText.value = "";
+//     noteTextField.value = "";
+//     postNewNote(titleText, noteText).then();
+//   });
+//   function postNewNote(titleText, noteText) {
+//     return fetch("http://localhost:3000/notes/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({
+//         title: titleText,
+//         note: noteText,
+//         done: false,
+//         created: moment().format()
+//       })
+//     }).then(response => response.json());
+//   }
 
+//Things still to do:
+// - Edit the note
+// - Style the hell out of this
 
-
-//Example I'm currently playing with:
-// print('delete ' + event.target.parentElement.dataset.todoId)
-
-// if(event.target.matches('.delete')){
-//     print(event.target)
-//     print(event.target.parentElement.dataset.todoId)     (Can magic it up if it has data-note-ID on the LI,)
-// }
-// })
 
 
 //======================Notes and other nonsense======================================================================
@@ -116,3 +180,126 @@ console.log("Nothing broken!")
 
 // const moment = require("moment")
 //Do I need this?
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This works and I'm about to break it for the edit feature.
+// let editTextBox = q("#note-text")
+// let editInput = q("edit-text")
+
+// function q(selector){
+//     return document.querySelector(selector)
+// }
+
+// function getAllNotes(){
+//     return fetch ("http://localhost:3000/notes/", {
+//     method: 'GET'
+//     })
+// .then(response=>response.json())
+// }
+
+// function createNotesHTML(notes){
+//     console.log(notes)
+//     let notesStr = '<ul id="notes-list">'
+//     for (const note of notes){
+//         notesStr+=createNoteHTML(note)
+//     }
+//     notesStr += '</ul>'
+//     return notesStr
+// }
+
+// function createNoteHTML(note){
+//     return `<li class="title" data-node-id="${note.id}">${note.title}</li>   <li data-note-id="${note.id}">${note.note} <button class="edit">Edit</button><button class="delete">Delete</button></li>`
+// }
+
+// function renderNotesList(notes) {
+//     const notesHTML = createNotesHTML(notes)
+//     const notesSection = document.querySelector('#notes')
+//     notesSection.innerHTML = notesHTML
+// }
+
+// q ("#new-note-form").addEventListener('submit', event=>{
+//     event.preventDefault();
+//     const titleTextField = q('#note-title').value
+//     const noteTextField = q('#note-text').value  //This line will eventually become multiple functions.  See right below.
+//     fetch("http://localhost:3000/notes/", {
+//         method: 'POST',
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({
+//             note:noteTextField, title:titleTextField, done:false, create:moment().format()})
+//     })
+// })
+// //This is what I was referring to:
+// // q('#new-todo-form').addEventListener('submit', event => {
+// //     event.preventDefault()
+// //     const todoTextField = q('#todo-text')
+// //     const todoText = todoTextField.value
+// //     todoTextField.value = ''
+// //     postNewTodo(todoText).then(renderNewTodo)
+// //   })
+
+// function postNewNote (titleText, noteText){
+//     return fetch('http://localhost:3000/notes/',{
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json'},
+//         body: JSON.stringify({note:noteText, title: titleText, note: noteText, done: false, created: moment().format()})
+//     })
+//     .then(response=>response.json())
+// }
+
+// function deleteThisNote (noteId){
+//     return fetch('http://localhost:3000/notes/' + noteId,{
+//         method: 'DELETE',
+//         headers: { 'Content-Type': 'application/json'},
+//         body: JSON.stringify({note:noteId, done: false, created: moment().format()})
+//     })
+//     .then(response=>response.json())
+// }
+
+// function deleteNote(){
+//     q('#notes').addEventListener('click', event=>{
+//         if (event.target.matches('.delete')){
+//             event.target.parentElement.classList.add("noteToDelete")
+//             let noteId = (event.target.parentElement.dataset.noteId)
+//             deleteThisNote(noteId)
+//         }   
+//     }
+//     )
+// }
+
+// function editThisNote (noteId){ //addElement??  <li data-note-id="${note.id}">${note.note}</li>
+//     return fetch('http://localhost:3000/notes/' + noteId,{
+//         method: 'PATCH',
+//         headers: { 'Content-Type': 'application/json'},
+//         body: JSON.stringify({note:"new body text", done: false, edited: moment().format()})
+//     })
+//     .then(response=>response.json())
+// }
+
+// function editNote(){
+//     q('#notes').addEventListener('click', event=>{
+//         if (event.target.matches('.edit')){
+//             event.target.parentElement.classList.add("noteToEdit")
+//             let noteId = (event.target.parentElement.dataset.noteId)
+//             editThisNote(noteId)
+//         }   
+//     }
+//     )
+// }
+
+// getAllNotes().then(createNotesHTML)
+// getAllNotes().then(renderNotesList)
+// deleteNote()
+// editNote()
+
+// console.log("Nothing broken!")
